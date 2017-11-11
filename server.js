1ceebfecc3029dacf6de9a1b1491c56650069562
salahuddin ansari <sansari150894@gmail.com>
2017-11-11 19:42:40 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const hbs=require('hbs');
 var app =express();
+const port=process.env.PORT||3000;
 app.set('view engine','hbs');
 
 app.use(express.static(__dirname+'/public'));
@@ -9,6 +10,6 @@ app.get('/',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log("");
+app.listen(port,()=>{
+    console.log(`Server is started at :${port}`);
 });
